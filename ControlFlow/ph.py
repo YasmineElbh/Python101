5dd5d4552fa7a9b0c2dd1dd1f91f09a5b7358b5c
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-01 21:56:06 +0100

    ph

---
+++ ControlFlow/ph.py
@@ -0,0 +1,16 @@
+#== equal to
+# != not equal to
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+#elif == short of (else if)
+
+ph = int(input('give me a value between 0 and 14 ' ))
+
+if ph > 7:
+    print('Basic')
+elif ph < 7:
+    print('Acidic')
+else:
+    print('Neutral')
