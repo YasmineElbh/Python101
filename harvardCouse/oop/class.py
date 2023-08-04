d6662c39c8151cbfb52e8513604300f57dae7bb1
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-05 00:36:59 +0100

    classes

--- harvardCouse/oop/class.py
+++ harvardCouse/oop/class.py
@@ -1,3 +1,5 @@
+#example1
+
 class Point():
     def __init__(self, input1, input2):
         self.x =  input1
@@ -5,3 +7,29 @@ class Point():
 
 p = Point(2, 8)
 print(p.x, p.y)
+
+#example2
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+    
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+    
+flight = Flight(3)
+
+people = ["yass", "james", "james1", "james 2"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"added {person} to flight succefully")
+    else:
+        print(f"no available seats for {person}")
