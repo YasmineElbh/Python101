1540103bbaaeeb5a0c5b3b08d14b01f0fd18593f
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-04 22:00:40 +0100

    loops

---
+++ harvardCouse/loops.py
@@ -0,0 +1,16 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+#range
+for i in range(7):
+        print(i)
+
+names = ["yass", "min", "ron"]
+
+for name in names:
+        print(name)
+
+char = "harry"
+
+for character in char:
+        print(character)
