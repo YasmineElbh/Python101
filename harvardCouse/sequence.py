03aa01cf2b591895097641174ce2e3258e3281a1
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-04 21:52:52 +0100

    sets

--- harvardCouse/sequence.py
+++ harvardCouse/sequence.py
@@ -3,8 +3,10 @@ name = "yasmine"
 print(name[5]) 
 
 #multiple sequences
-names = ['yas', 'min', 'harry']
+names = ['yas', 'min', 'harry', 'aa']
 print(names[2])
 
-#
-names.append("draco")
+
+names.append("draco") #adding value on list
+names.sort() #It sorts list elements in either ascending or descending order. 
+print(names)
