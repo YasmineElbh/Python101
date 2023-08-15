7ac7474b912945f7b9fcb79d4861edd15f2dfcbe
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-15 14:38:33 +0100

    list

---
+++ list/list.py
@@ -0,0 +1,13 @@
+x = [3, 5, 'd']
+
+x[0] #3
+len(x) #3
+x.insert(2, 'yasmine')
+x.remove('yasmine')
+x.pop() #remove the last element
+del x #remove the whole element
+x.sort() #sort the elements
+x.reverse()
+x.append(10) #append the first element
+y = x.copy() #copy the x elements to y
+x.count(3)
---
+++ projects/truth_dare.py
@@ -0,0 +1,28 @@
+#truth or dare proect
+
+#1. Input how many people are gonna play.
+#2. Enter the players names.
+#4. Choose Truth or Dare.
+#5. At the end of the program, computer gives you a Truth or Dare question.
+
+# numb_player = input('how many player gonna play?')
+# numb_player = int(numb_player)
+
+# list_players = []
+# answer = 'yes'
+
+# player_name = input("what's the name o the player")
+# list_players.append(player_name)
+
+# while (answer == 'yes'):
+#     player = random(list_players)
+#     print('its', player,'turn')
+
+# truth_dare = input('type truth to choose or dare')
+
+# truth_items = ['What is your worst habit?', 'Do you sing in the shower?', 'Are you scared of the dark?']
+# dare_items  = ['dance in the street like crazy.', 'Bark like a dog loudly.', 'take your shirt off spin it.']
+
+# if str(truth_dare) == 'truth':
+#     print((truth_items))
+
