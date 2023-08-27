cf45b255a99fb533681f967a04a98ce8f3d012c8
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-27 21:59:56 +0100

    static methods

--- oop/staticMethods.py
+++ oop/staticMethods.py
@@ -12,8 +12,27 @@ class Math:
     @staticmethod
     def PI():
         return 3.14
-    
-x = Math.add(5, 10)
-y = Math.add5(x)
-print(x, y)
 
+#another example
+class Pizza:
+    def __init__(self, radius, ingredient):
+        self.radius = radius
+        self.ingredients = ingredient
+
+    def __str__(self):
+        return f'pizza ingredients are {self.ingredients}'
+
+    def area(self):
+        return Pizza.circle_area(self.radius)
+                
+@staticmethod
+def circle_area(r):
+    return r ** 2 * Math.PI()
+        
+# x = Math.add(5, 10)
+# y = Math.add5(x)
+# print(x, y)
+
+p = Pizza(6, ['mozarilla', 'tomatoes'])
+print(p.area())
+print(Pizza.circle_area(4))
