04e74c913b1c0d205cbe9b33ab60648a7d9918c0
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-27 22:35:18 +0100

    oop

--- oopWithFreeCodeCamp/main.py
+++ oopWithFreeCodeCamp/main.py
@@ -1,10 +1,19 @@
 #methods: mean two functio that are inside the classes
+#__init__:is a method with a unique name that you need to call it the way it is intentionally in order to use its special futures
+
 class item:
+    def __init__(self, name, price, quantity):
+    #  print(f'An instence created: {name}')
+        self.name = name
+        self.price = price
+        self.quantity = quantity
     def Calculates_total_price(self, x, y):
         return x * y
 
-item1 = item()
-item1.name = 'phone'
-item1.price  = 1000
-item.quantity = 4
-print(item1.Calculates_total_price(item1.price, item1.quantity))
+
+item1 = item("sams", 100, 5)
+# item1.price  = 1000
+# item.quantity = 4
+#print(item1.Calculates_total_price(item1.price, item1.quantity))
+print(item1.name)
+print(item1.price)
