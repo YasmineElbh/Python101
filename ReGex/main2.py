03ce9df666363cc232f59f4207eee421aad40ac0
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-31 21:51:34 +0100

    regex

--- ReGex/main2.py
+++ ReGex/main2.py
@@ -10,3 +10,8 @@ print(pattern.match('helloworld'))
 #3-5 digits
 #one symbol
 #up to two uppercase characte(optional)
+pattern = re.compile("^[a-z]{3}[0-9]{3,5}[^a-zA-Z0-9]{1}[A-Z]{0,2}$")
+print(pattern.search('Hello257#6'))
+print(pattern.search('lll44511.k'))
+print(pattern.match('hel123456#JJ'))
+
