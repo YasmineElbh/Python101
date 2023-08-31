9f72b91f3d7cbfe86bf83c11411466f6987900fa
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-08-31 15:43:19 +0100

    regex

---
+++ ReGex/README.md
@@ -0,0 +1,24 @@
+<***What is Reguar expression***>
+is a powerful language tool for machine text patterns
+<!--the most common uses of regular xpession-->
+~Search a string
+~finding a string
+~Break string into a sub strings
+~Replace part of a string
+
+<***Why you should use RegularExpression***>
+~they do a lot with less
+~Standing out from the crowd
+~they are super fast
+
+<***R Most Commonly Methods***>
+re.match():checks for a match only a the beginning of the string
+re.search():checks for a match anywhere in the string
+re.findall():find all instances of words in a string
+re,split():split and separate a string into multiple string
+re.sub():search a pattern and replace with a new sub string
+re.compile():convert a pattern into objects
+
+<!--Patterns Examples-->
+    r'(\w+)(\w+)'
+r ==> Raw string notation
---
+++ ReGex/main.py
@@ -0,0 +1,65 @@
+#Example 1
+import re
+pattern = 'hello'
+sequence = 'hello world'
+if re.match(pattern, sequence):
+    print('match!')
+else:
+    print('not a match!')
+
+#Example 2
+result = re.match(r'AV', 'AV Analytics vidhaya AV')
+print(result.start())
+print(result.end())
+
+#Example3
+r = re.match(r'AV', 'AV Analytics vidhaya AV')
+print(r)
+
+#Example4
+#validate a phone number(phone nummber must be of 10 digits and starts with 8 or 9)
+
+li = ['9999999999', '999999-99', '99999x999']
+for val in li:
+    if re.match(r"[8-9]{1}[0-9]{9}", val) and len(val) == 10:
+        print('yes')
+    else:
+        print('no')
+
+#Example5
+#a blackslash is used to define special characters in regx
+
+rr = re.match('\\\\', '\\author')
+print(rr.group())
+
+#unicode language
+        
+if re.match(u'ياسمين', u'ياسمين', re.UNICODE):
+    print('match!')
+else:
+    print('not a match!')
+
+#Search
+#check for a match anywhere on the string
+result = re.search(r'AV', 'AV Analytics vidhaya AV')
+print(result)
+print(result.group(0))
+
+#/= any char but \n
+red = re.search(r'..g', 'piiig')
+print(red)
+print(red.group(0))
+
+#d = digit char, \w = word char
+res = re.search(r'\d\d\d', 'p123')
+print(res.group(0))
+res = re.search(r'\w\w\w', '@!abd')
+print(res.group(0))
+
+#i+ = one or more i's, as many as possible
+result  = re.search(r'pi+', 'piiiiiiiif')
+if result:
+    print(result.group())
+
+#\s* = zero or more whitespace chars, ##here look for 3 digits, possibly separted y whitespace
+result = re.search(r'\d\s')
