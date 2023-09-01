2a5e55bd02e4d08352a6ba73b320de3025f05881
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-09-01 17:51:24 +0100

    oop

--- oopWithFreeCodeCamp/classe/main.py/main1.py
+++ oopWithFreeCodeCamp/classe/main.py/main1.py
@@ -1,6 +1,7 @@
 #instance attribut: which are defined in the __init__() function and are somewhat dynamic because they can have different values in each object.
 #Class attribut: remain the same for every object and are defined outside the __init__()
 #assert: is a statement keyword that is used t chec if there is a match between what is happening to your expectations
+
 class item:
     pay_rate = 0.8
     def __init__(self, name: str, price: float, quantity = 0):
@@ -15,7 +16,9 @@ class item:
         
     def Calculates_total_price(self):
         return self.price * self.quantity
-
+    
+    def apply_discount(self):
+        self.price = self.price * item.pay_rate
 
 item1 = item("sams", 100, 5)
 item2 = item("sas", 200, 3)
@@ -26,3 +29,6 @@ print(item1.pay_rate)
 print(item.__dict__) #all the attributes for class level 
 print(item1.__dict__) #all the attributes for instance level 
 
+
+item1.apply_discount()
+print(item1.price)
